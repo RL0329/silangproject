64f56a7c24574ef7c6ca83c571c44189364b627c
niel <annie.gerodias@gmail.com>
2018-06-17 22:33:21 +0800

    Commited

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Annielyn :D
+                    Hello Annie :)
                 </div>
 
                 <div class="links">
